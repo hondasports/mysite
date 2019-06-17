7b8c68c870157a49d95a68100db9f6974e426149
MIYAMOTO TATSUYA <hondasports@gmail.com>
2019-06-17 21:44:26 +0900

    Toolbalを削除し、cardのトップページに変更中。

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">Tatsuya Miyamoto</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn flat><router-link to="/about">about</router-link></v-btn>
-        <v-btn flat><router-link to="/skill">skill</router-link></v-btn>
-        <v-btn flat><router-link to="/content">content</router-link></v-btn>
-        <v-btn flat><router-link to="/contact">contact</router-link></v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
     <v-content>
       <router-view/>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <v-card>
+              <v-btn flat><router-link to="/about">about</router-link></v-btn>
+            </v-card>
+            <v-card>
+              <v-btn flat><router-link to="/skill">skill</router-link></v-btn>
+            </v-card>
+            <v-card>
+              <v-btn flat><router-link to="/content">content</router-link></v-btn>
+            </v-card>
+            <v-card>
+              <v-btn flat><router-link to="/contact">contact</router-link></v-btn>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
+    <v-footer class="pa-3">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }} Tatsuya Miyamoto @hondasports8</div>
+    </v-footer>
   </v-app>
+
 </template>
 
 <script>
