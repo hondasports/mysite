7d1eb0a348177b8f590ac6dbf58ce4890d4db9be
MIYAMOTO TATSUYA <hondasports@gmail.com>
2019-07-04 21:33:21 +0900

    Added explain subtitle.

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,16 @@
 <template>
   <v-layout>
     <v-flex>
+      <v-card flat>
+        <v-card-title>
+          Programming is one of my life...
+        </v-card-title>
+        <v-card-text>
+          プログラミングは人生の一部と書いたのは、昔異業種への転職も含めて考えていた時に友人からもらったスマホアプリの開発をしていました。
+          調べならが徐々にできて行くアプリに楽しさを覚え、それ以来プログラミングが楽しい事、人生の一部になりました。
+          そんな気持ちをサブタイトルに込めています。
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
  </template>
