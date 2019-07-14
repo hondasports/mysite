7916d91da11a0f25457781c44189baf53e18040b
hondasports <hodnasports@gmail.com>
2019-07-14 16:57:50 +0900

    [WIP] added redirect to index when page not foud.

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import index from './views/Index.vue'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'hash',
+    mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
             path: '/',
